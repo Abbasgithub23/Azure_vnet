3f345d96616e2fc38cc42dd7575ddbda4308adb1
Mohammed Abbas <Mohammed.Abbas@jda.com>
2018-12-11 19:32:39 +0530

    Vnet Modified

--- main.tf
+++ main.tf
@@ -7,13 +7,22 @@ provider "azurerm" {
 }
 
 
-# Create subnet
+# Create subnet-1
 resource "azurerm_subnet" "myterraformsubnet" {
     name                 = "demo-subnet"
     resource_group_name  = "${azurerm_resource_group.myterraformgroup.name}"
     virtual_network_name = "${azurerm_virtual_network.myterraformnetwork.name}"
     address_prefix       = "10.0.1.0/24"
 }
+
+# Create subnet-2
+resource "azurerm_subnet" "myterraformsubnet1" {
+    name                 = "demo-subnet1"
+    resource_group_name  = "${azurerm_resource_group.myterraformgroup.name}"
+    virtual_network_name = "${azurerm_virtual_network.myterraformnetwork.name}"
+    address_prefix       = "10.0.2.0/24"
+}
+
 
 # Create a resource group if it does not exist
 resource "azurerm_resource_group" "myterraformgroup" {
